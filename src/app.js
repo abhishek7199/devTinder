425cfd6448f0594bec3b4210ceb497f4db749348
Abhishek Kumar <abhishek77882@gmail.com>
2025-11-01 18:18:36 +0530

    main

--- src/app.js
+++ src/app.js
@@ -2,13 +2,26 @@ const express = require("express");
 
 const app = express();
 
-app.use("/app", (req, res) => {
-    res.send("Hello World!");
+// app.use("/app", (req, res) => {
+//     res.send("Hello World!");
+// });
+
+// app.use("/test", (req, res) => {
+//     res.send("from /test");
+// });
+
+// app.use("/some", (req, res) => {
+//     res.send("This is from /some");
+// });
+
+
+app.get("/test", (req, res) => {
+    res.send({
+        firstName: "Abhishek",
+        lastName: "Kumar"
+    });
 });
 
-app.use("/test", (req, res) => {
-    res.send("from /test");
-});
 
 app.listen(7777, () => {
     console.log("Server is successfully listening on port 7777");
