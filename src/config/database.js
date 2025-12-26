93923ff9889064175da434b74a7ed205ec9a9198
Abhishek Kumar <abhishek77882@gmail.com>
2025-12-27 01:06:16 +0530

    changes in gitignore

--- src/config/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://abhishek77882:abhi172@cluster0.vutj8rr.mongodb.net/?appName=Cluster0");
-}
-
-module.exports = connectDB;
